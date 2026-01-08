d92c92d0e0da1fc74ae5fb40c967112823b801f2
Sibibalasankar <sibibalasankar1915@gmail.com>
2026-01-08 15:16:59 +0530

    UI Added

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
+from flask import Flask,render_template,request
+
 
 load_dotenv()
+
+app=Flask(__name__)
 
 model=ChatGoogleGenerativeAI(
     model='gemini-2.5-flash',
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
-print("Bot:")
-while True:
-    user_input=input("\nYou:")
+@app.route("/",methods=["GET","POST"])
+def Ai():
+    reply=""
+    if request.method=="POST":
+        user_msg=request.form.get("msg")
+        reply=model.invoke(user_msg).content
+    return render_template("index.html",reply=reply)
+
+
+if __name__ == "main":
+    app.run()
+
+# print("Bot:")
+# while True:
+#     user_input=input("\nYou:")
     
     
-    if user_input.lower() in ['exit','quit','bye']:
-        print("Good Bye!")
-        break
-    response = model.invoke(user_input)
-    print(response.content)
+#     if user_input.lower() in ['exit','quit','bye']:
+#         print("Good Bye!")
+#         break
+#     response = model.invoke(user_input)
+#     print(response.content)
+
+
