88371e526b3d3523f695107d00c9d81a02446dbc
Sibibalasankar <sibibalasankar1915@gmail.com>
2026-01-08 14:03:56 +0530

    Basic Version is Build

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
+model=ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash',
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+print("Bot:")
+while True:
+    user_input=input("\nYou:")
+    
+    
+    if user_input.lower() in ['exit','quit','bye']:
+        print("Good Bye!")
+        break
+    response = model.invoke(user_input)
+    print(response.content)
